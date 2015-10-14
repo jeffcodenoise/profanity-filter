3e52cc2807ca72555e000519348e74657608bdb7
Jeff Nehlsen <jeff.nehlsen@gmail.com>
2015-10-14 00:31:16 -0700

    more improvements to matching.

--- spec/filter_spec.js
+++ spec/filter_spec.js
@@ -4,8 +4,8 @@ var assert = require('assert');
 var filter = require('../lib/filter.js');
 
 var fixtures = {
-  dirtyString: 'That darn UX change was such a pain in the butt. It was f-ing crappy.',
-  cleanString:  'That silly UX change was such a pain in the elbow. It was horse back riding chicken.',
+  dirtyString: 'I do not give a fudge. That darn UX change was such a pain in the butt. It was fudging crappy.',
+  cleanString:  'I do not give a chicken. That silly UX change was such a pain in the elbow. It was horse back riding chicken.',
   seed: {
     'replacements': {
       'body-part': ['elbow'],
@@ -16,7 +16,11 @@ var fixtures = {
     'words': {
       'darn': 'adjective',
       'butt': 'body-part',
-      'f-ing': 'verb',
+      'butthole': 'body-part',
+      'fudging': 'verb',
+      'fudge': 'noun',
+      'buck': 'noun',
+      'bucking': 'verb',
       'crappy': 'noun'
     }
   }
@@ -28,6 +32,20 @@ describe ("clean", function() {
   it ("should replace the string", function() {
     var filteredString = filter.clean(fixtures.dirtyString);
     assert.equal(filteredString, fixtures.cleanString);
+  });
+
+  it ("should work with fancy stuff", function() {
+    var badString = "The buck was bucking.";
+    var expectedString = "The chicken was horse back riding.";
+    var filteredString = filter.clean(badString);
+    assert.equal(filteredString, expectedString);
+  });
+
+  it ("should work with butt and butthole", function() {
+    var badString = "That butthole has a butt.";
+    var expectedString = "That elbow has a elbow.";
+    var filteredString = filter.clean(badString);
+    assert.equal(filteredString, expectedString);
   });
 
   it ("should work with lots of bad words", function() {
